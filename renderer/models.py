4bf148bd75dace7e318d1f502b058068a8239cec
Your Name <toomelaaimar@gmail.com>
2024-12-21 11:17:41 +0200

    added Poster to models.py

--- renderer/models.py
+++ renderer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Poster(models.Model):
+    url = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.url
